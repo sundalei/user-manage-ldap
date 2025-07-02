b704a69616f7b5c2a3b166994e562a55301d2508
sundalei <sundalei1988@gmail.com>
2025-07-02 23:03:00 +0800

    Init commit

---
+++ src/main/java/com/example/DemoToShowcaseSpringLdapApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoToShowcaseSpringLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoToShowcaseSpringLdapApplication.class, args);
+	}
+
+}
