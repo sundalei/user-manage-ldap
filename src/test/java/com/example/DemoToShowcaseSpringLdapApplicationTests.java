620fc3f393b432b89bc00521d2ee0907c7ce25eb
sundalei <sundalei2011@163.com>
2025-07-12 13:15:08 +0800

    docs(test): add package-info and fix test method formatting
    Add package-info.java to document test package purpose
    Fix whitespace in contextLoads() test method

--- src/test/java/com/example/DemoToShowcaseSpringLdapApplicationTests.java
+++ src/test/java/com/example/DemoToShowcaseSpringLdapApplicationTests.java
@@ -7,5 +7,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoToShowcaseSpringLdapApplicationTests {
 
   @Test
-  void contextLoads() {}
+  void contextLoads() { }
 }
---
+++ src/test/java/com/example/package-info.java
@@ -0,0 +1,2 @@
+/** Provides the test classes for the user management application. */
+package com.example;
