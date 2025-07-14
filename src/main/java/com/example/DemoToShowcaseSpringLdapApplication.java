622d2fed2ea60080ca75df5c00d8253a3e9d2cab
sundalei <sundalei2011@163.com>
2025-07-14 21:59:53 +0800

    style: disable checkstyle for final parameters in main method

--- src/main/java/com/example/DemoToShowcaseSpringLdapApplication.java
+++ src/main/java/com/example/DemoToShowcaseSpringLdapApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoToShowcaseSpringLdapApplication {
 
+  // CHECKSTYLE:OFF: FinalParameters
   public static void main(String[] args) {
+    // CHECKSTYLE:ON: FinalParameters
     SpringApplication.run(DemoToShowcaseSpringLdapApplication.class, args);
   }
 }
