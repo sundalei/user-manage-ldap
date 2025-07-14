2fe411a7a1f8300b285dff5c382d5f3b72dcc62f
sundalei <sundalei2011@163.com>
2025-07-14 22:50:11 +0800

    docs(repository): add javadoc comments to user repository classes
    Add detailed javadoc comments to UserRepository, UserRepositoryCustom and UserRepositoryImpl classes to improve code documentation

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -9,7 +9,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends LdapRepository<User>, UserRepositoryCustom {
 
+  /**
+   * Finds a user by their UID.
+   *
+   * @param uid UID of the user
+   * @return Optional containing the user if found, or an empty Optional otherwise
+   */
   Optional<User> findByUid(String uid);
 
+  /**
+   * Finds users by their surname.
+   *
+   * @param sn Surname of the users
+   * @return List of users with the given surname
+   */
   List<User> findBySn(String sn);
 }
